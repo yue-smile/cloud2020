ac3bc400ea459adf51c2050517f781154af68ede
wangyue <wy10131013>
2021-12-31 18:27:46 +0800

    Hystrix仪表盘展示 需要新建一个工程新增如下注解 @EnableHystrixDashboard 7色一圈（绿色...红色）一线

--- cloud-provider-hystrix-payment8001/src/main/java/com/wangyue/springcloud/PaymentHystrixMain8001.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/wangyue/springcloud/PaymentHystrixMain8001.java
@@ -1,10 +1,13 @@
 package com.wangyue.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -14,4 +17,13 @@ public class PaymentHystrixMain8001 {
            SpringApplication.run(PaymentHystrixMain8001.class, args);
       }
 
+      @Bean
+      public ServletRegistrationBean getServlet(){
+          HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+          ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+          registrationBean.setLoadOnStartup(1);
+          registrationBean.addUrlMappings("/hystrix.stream");
+          registrationBean.setName("HystrixMetricsStreamServlet");
+          return registrationBean;
+      }
 }
