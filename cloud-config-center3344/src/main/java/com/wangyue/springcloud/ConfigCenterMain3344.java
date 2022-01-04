8fa9495e43784e25db8a7722b46b9b002e3eae95
wangyue <wy10131013>
2022-01-04 18:34:07 +0800

    SpringCloud Config配置中心 ConfigServer与git同步 其他微服务与ConfigServer同步
    @RefreshScope注解，可不重启服务刷新配置
    需要暴露监控接口，同时需要手动发送一次post请求到需要刷新配置的微服务
    因此需要引入消息总线

---
+++ cloud-config-center3344/src/main/java/com/wangyue/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,16 @@
+package com.wangyue.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+
+}
---
+++ cloud-config-client3355/src/main/java/com/wangyue/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,18 @@
+package com.wangyue.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope //修改配置文件后自动刷新  yml配置暴露监控端口management  文件修改后还需要一次post请求刷新  curl -X POST "http://localhost:3355/actuator/refresh"
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping(value = "/getInfo")
+    public String getInfo(){
+        return configInfo;
+    }
+}
