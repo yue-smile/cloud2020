6c445ee9df13fcd2e8e004e8ac67b7526a5bc462
wangyue <wy10131013>
2022-01-04 15:25:30 +0800

    gateway断言Predicate 九种校验示例 After Before Between Cookie Header Host Method Path Query

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,12 @@
+import sun.applet.Main;
+
+import java.time.ZonedDateTime;
+
+public class T2 {
+
+    public static void main(String[] args) {
+        ZonedDateTime zbj = ZonedDateTime.now();
+        System.out.println(zbj);
+    }
+
+}
