159c3fd77d60f44916546e0202a2dd70baec9625
wangyue <wy10131013>
2022-01-05 15:27:57 +0800

    SpringCloud Bus消息总线 支持rabbitmq和kafka git>配置中心>各个微服务 需要打开actuator监控 无需重启发送一个post请求全局或定点刷新配置

---
+++ cloud-config-client3366/src/main/java/com/wangyue/springcloud/ConfigMain3366.java
@@ -0,0 +1,14 @@
+package com.wangyue.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigMain3366 {
+     public static void main(String[] args) {
+           SpringApplication.run(ConfigMain3366.class, args);
+      }
+     
+}
