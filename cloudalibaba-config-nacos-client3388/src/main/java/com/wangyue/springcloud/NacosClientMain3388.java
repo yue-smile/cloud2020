1ea5b4e6adfd54f8c6c2edcb988909d98006fa0a
wangyue <wy10131013>
2022-01-06 18:32:45 +0800

    SpringCloudalibaba nacos 配置中心 pom spring-cloud-starter-alibaba-nacos-config yml spring.cloud.nacos.config.server-addr: localhost:8848和file-extension 管理台文件名 ${spring.application.name}-${spring.profile.active}.${spring.cloud.nacos.config.file-extension} yml中配置服务中心地址，nacos管理页面根据前缀后缀的规则新建配置文件然后发布即可。

---
+++ cloudalibaba-config-nacos-client3388/src/main/java/com/wangyue/springcloud/NacosClientMain3388.java
@@ -0,0 +1,14 @@
+package com.wangyue.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosClientMain3388 {
+     public static void main(String[] args) {
+           SpringApplication.run(NacosClientMain3388.class, args);
+      }
+
+}
---
+++ cloudalibaba-config-nacos-client3388/src/main/java/com/wangyue/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,18 @@
+package com.wangyue.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope //支持nacos的动态刷新功能
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping(value = "getConf")
+    public String getConfig(){
+        return configInfo;
+    }
+}
